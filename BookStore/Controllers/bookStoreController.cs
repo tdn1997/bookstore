4c1b245fc8039677704429a0228c0fd945c96d67
thuan <nauhtyud@gmail.com>
2021-03-24 19:12:47 +0700

    thien pro max

---
+++ BookStore/Controllers/bookStoreController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class bookStoreController : Controller
+    {
+        dataBookStoreDataContext choThuan = new dataBookStoreDataContext();
+        private List<SACH> laySachMoi(int count)
+        {
+            return choThuan.SACHes.OrderByDescending(a => a.Ngaycapnhat).Take(count).ToList();
+        }
+        public ActionResult Index()
+        {
+            var sachmoi = laySachMoi(5);
+            return View(sachmoi);
+        }
+    }
+}
